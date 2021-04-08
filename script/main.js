67af7821f6f6ea6d7f6f4cdb12c69d1b944d3817
soup423 <kent35103@gmail.com>
2021-04-08 15:09:50 +0300

    ver 1.1

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ let score = 0;
 do {
 	++score;
 	if (score % 3 === 0 && score % 5 === 0) {
-		console.log("FizzBuzz")
+		console.log('FizzBuzz')
 		continue;
 	}
 	if (score % 3 === 0) {
@@ -13,9 +13,15 @@ do {
 		console.log('Buzz')
 		continue;
 	}
-
-
 	console.log(score)
 }
 while (score < 100);
+
+
+let stroka = 'Лето удалось очень жарким, было много ягод и фруктов, лучшим местом для отдыха была речка.'
+console.log(stroka.replaceAll(',', '.'));
+
+
+let strokaImena = 'Антон, Дима и Женя пришли на День рождения сразу, а Миша пришел позже';
+console.log('На день рождения пришли:', strokaImena.slice(0, 5), ',', strokaImena.slice(7, 11), ',', strokaImena.slice(14, 18), ',', strokaImena.slice(52, 56))
 
